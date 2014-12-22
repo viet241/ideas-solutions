82fd0ce9bc3d4bc8a9afd736fe87a431b0221709
Viet <viet.tran@bluecomgroup.com>
2014-12-22 16:41:40 +0700

    empty

---
+++ assets/config.rb
@@ -0,0 +1,13 @@
+# note: this should never truly be refernced since we are using relative assets
+#http_path = "/skin/frontend/rwd/default/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "images"
+javascripts_dir = "js"
+relative_assets = true
+line_comments = false
+#output_style = :compressed
+output_style = :expanded
+# output_style = :compact
+environment = :production
+# preferred_syntax = :scss
---
+++ assets/js/application.js
@@ -0,0 +1,66 @@
+$(function(){
+	"use strict";
+
+	$.fn.extend({
+		scrollTo : function(speed, easing, callback) {
+			var anchor = $(this).length ? $(this).offset().top : 0;
+			$('html,body').stop().animate({'scrollTop': anchor}, speed || 0, easing, callback);
+		}
+	});
+
+
+	// variables
+	var $win = $(window), 
+		$scrollTop = 0,
+		$scrollDuration = 600;
+
+	$(".svg-inject").svgInject();
+
+	$(".box.show").removeClass("show");
+	
+	$(document).on('click', "#menu-toggle", function(e) { // menu toggle
+		e.preventDefault();
+		$(this).toggleClass('active').next().addClass("ani-off").slideToggle(300);
+		return !1;
+	})
+
+	.on('click', "a[href^='#']", function(e) { // anchor links click / smooth scroll
+		e.preventDefault();
+		var target = this.hash;
+		$(target).scrollTo($scrollDuration, 'swing', function(){
+			window.location.hash = target;
+		});
+	});
+
+
+	
+	$win.on('scroll', function(){ // window scroll event
+
+		$scrollTop = $win.scrollTop();
+		var $winHeight = $win.height();
+
+		// navigation fixed
+		$scrollTop + 1 >= $("#intro").height() ? $("#header").addClass("fixed") : $("#header").removeClass("fixed");
+
+
+		// move background / show sidebox
+		$("#main > section").each(function(){
+			var $screen = $(this),
+			$goOver = $scrollTop - $screen.offset().top,
+			$box = $screen.find(".box");
+
+			$screen.css('background-position', $goOver > 0 ? ('50% -' + $goOver / 5 + 'px') : '50% 50%');
+
+			if($box.length)
+				$scrollTop + $winHeight >= $box.offset().top ? $box.addClass("show") : $box.removeClass("show");
+		});
+
+	})
+
+	.on('hashchange', function() { // window hashchange / smooth scroll
+		var target = location.hash;
+		$(target).scrollTo($scrollDuration, 'swing', function(){
+			window.location.hash = target;
+		});
+	});
+});
